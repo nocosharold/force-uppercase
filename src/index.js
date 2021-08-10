fafd6f1c93554b6d84866ad20d89e7d0f7e9a1ff
nocosharold <nocosharold@gmail.com>
2021-08-10 10:58:23 +0800

    force UPPERCASE

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  
+    // A required React method when using class-based component.
+    constructor(props) {
+      super(props);
+      this.state = {character:""};
+    }
+    
+    toUpper(e) {
+      this.setState({ character:e.target.value.toUpperCase() });
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        return (
+            <div className="container">
+              <section class="content bgcolor_ruri">
+                <div class="input input_ruri">
+                  <input type="text" name="text" id="text" value={this.state.character} onChange={(e)=>{this.toUpper(e)}} required />
+                  <label for="ruri" class="input_label_ruri">
+                      <span class="input_label_content">Force UPPERCASE</span>
+                  </label>
+                </div>
+              </section>
+            </div>
+        );
+    };
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
